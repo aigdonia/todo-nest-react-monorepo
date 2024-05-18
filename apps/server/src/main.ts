0050e64ed6fcbc4590b55f33d1dc1bd3e86e77ca
Ahmed Gaber <aigdonia@gmail.com>
2024-05-18 22:41:17 +0300

    chore: revert serverless flow for nest

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,26 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import serverlessExpress from '@codegenie/serverless-express';
-
-let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   await app.listen(4001);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
-
-/**
- * Handler for lambda function
- */
-export const handler = async (event: any, context) => {
-  if (!server) {
-    const nestApp = await NestFactory.create(AppModule);
-    nestApp.enableCors();
-    await nestApp.init();
-    server = serverlessExpress({ app: nestApp.getHttpAdapter().getInstance() });
-  }
-
-  return server(event, context);
-};
