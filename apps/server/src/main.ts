95a6e3c77040f9bcd0b6e5fe1318f5608caae676
Ahmed Gaber <aigdonia@gmail.com>
2024-05-18 15:03:02 +0300

    chore: delpoy nest app pipeline

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@codegenie/serverless-express';
+
+let server: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,3 +10,17 @@ async function bootstrap() {
   await app.listen(4001);
 }
 bootstrap();
+
+/**
+ * Handler for lambda function
+ */
+export const handler = async (event: any, context) => {
+  if (!server) {
+    const nestApp = await NestFactory.create(AppModule);
+    nestApp.enableCors();
+    await nestApp.init();
+    server = serverlessExpress({ app: nestApp.getHttpAdapter().getInstance() });
+  }
+
+  return server(event, context);
+};
